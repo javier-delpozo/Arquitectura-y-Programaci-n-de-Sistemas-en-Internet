1f3b75d30ed98e1c53aae987b7c9face01690e0d
javier-delpozo <43136807+javier-delpozo@users.noreply.github.com>
2022-10-17 17:56:47 +0200

    Add files via upload

---
+++ Práctica1/main.ts
@@ -0,0 +1,136 @@
+//PRÁCTICA 1 JAVIER DEL POZO MARIO PARRA
+function StrArrNumb(a:any[]){
+
+  let newitem: number; 
+  const output:number[]=[];
+  
+  for (let i=0; i<a.length; i++){
+    
+    if (typeof a[i] =="string"){
+      newitem=parseInt(a[i]);
+      output[i]=newitem;
+    } 
+    else if (typeof a[i] =="number") {
+      output[i]=a[i];
+    }
+    
+
+    
+  }
+   return output 
+  }
+
+
+function PlaneArr(a:number[]){
+  const out=a.flat(Infinity);
+  return out
+}
+
+
+function HowManyZeros(a:number[] ){
+
+  let i: number
+  let totalzeros:number
+  let j: number
+  const output:number[]=[];
+  
+  i=0;
+  totalzeros=0;
+  j=0;
+   while (i<a.length && totalzeros<=1 ){
+
+    if (a[i]==0){
+      totalzeros=totalzeros+1;
+      j=i;
+      i++
+
+      
+    }
+    else{
+      i++
+    }
+    
+  }
+  output[0]=totalzeros;
+  
+  output[1]=j;
+       
+  return output 
+  }
+
+
+
+function DotProduct(a:number[],pos:number){
+  let result:number;
+  
+  result=1;
+  
+  for (let i=0; i<pos;i++){
+    result=result*a[i];
+    
+  }
+  
+
+  for (let j=pos+1; j<a.length;j++){
+    result=result*a[j];
+  }
+  return result
+}
+
+
+
+function ArrayModify(nainp:any[]){
+  
+  const aout: number[]=[];
+  let limite:number;
+  let ainp:number[]=[];
+  
+  ainp=StrArrNumb(nainp);
+  ainp=PlaneArr(ainp);
+  
+  aout[0]=DotProduct(ainp,0);
+      
+  if (HowManyZeros(ainp)[0]==0){
+    for (let i=1; i< ainp.length; i++){
+      aout[i]=aout[i-1]*ainp[i-1]/ainp[i];
+    }
+        
+  }
+
+        
+      
+  else if (HowManyZeros(ainp)[0]==1){
+    limite=(HowManyZeros(ainp))[1];
+    for(let k=0; k<limite;k++){
+      aout[k]=0;
+    }
+          
+    aout[limite]=DotProduct(ainp,limite)
+            
+    for(let l=limite+1; l<ainp.length;l++){
+      aout[l]=0;
+    }
+  }
+  else {
+    for(let k=0; k<ainp.length;k++){
+      aout[k]=0;
+    }
+          
+  }    
+
+  return aout;
+}
+
+const ejemplo: number[]=[1,2,3];
+const ejemplo2: any[]=[1,[2,[3,4]]];
+const ejemplo3: string[]=["1","2","3"];
+const ejemplo4: number[]=[1,2,3,0];
+const ejemplo5: number[]=[1,2,0,3,0];
+
+console.log(ArrayModify(ejemplo))
+console.log(ArrayModify(ejemplo4))
+console.log(ArrayModify(ejemplo5))
+console.log(PlaneArr(ejemplo2))
+console.log(StrArrNumb(ejemplo3))
+      
+   
